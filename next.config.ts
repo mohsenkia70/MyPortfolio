f66da2640d80edc0c7e71978d1fb4d0d7aa38cc8
mohsen <mohsenkia70@gmail.com>
2025-12-26 23:13:17 +0330

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',           // مهم‌ترین خط — سایت استاتیک می‌شه
+//   trailingSlash: true,        // برای لینک‌ها بهتر کار کنه
+//   images: {
+//     unoptimized: true         // GitHub Pages از بهینه‌سازی تصویر Next.js پشتیبانی نمی‌کنه
+//   },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',           // مهم‌ترین خط — سایت استاتیک می‌شه
-  trailingSlash: true,        // برای لینک‌ها بهتر کار کنه
-  images: {
-    unoptimized: true         // GitHub Pages از بهینه‌سازی تصویر Next.js پشتیبانی نمی‌کنه
-  },
+  output: 'export',
+  basePath: '/MyPortfolio',
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
